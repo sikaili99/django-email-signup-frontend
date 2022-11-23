f55192182fcf609e42c87300ff804a049d915583
Mathewsmusukuma <sikaili99@gmail.com>
2022-11-23 16:38:16 +0200

    Redux store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
+import { createBrowserHistory } from "history"
 import { Provider } from "react-redux";
-import createHistory from "history/createBrowserHistory";
 import { store } from "./store";
 import App  from "./App";
 import "./styles.css";
 import "./login.css";
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
